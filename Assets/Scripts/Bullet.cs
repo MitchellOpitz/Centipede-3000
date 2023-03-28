f13230a8740f7e41ba60583122785a3d229e7560
Mitchell Opitz <mitchell@mitchellopitz.net>
2023-03-27 21:05:43 -0500

    fix - Destroy bullet offscreen

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -3,13 +3,24 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public float speed = 10f;
-    
+    public float maxY = 10f; // max y value before bullet is destroyed
+
     private Rigidbody2D rb;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         rb.velocity = transform.up * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // check if bullet is beyond maxY value
+        if (transform.position.y > maxY)
+        {
+            Destroy(gameObject);
+        }
     }
 
     void OnTriggerEnter2D(Collider2D other)
