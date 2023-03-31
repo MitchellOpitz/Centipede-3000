dfd99e27d4c728454f457ac2300e6830dc2f6a35
Mitchell Opitz <mitchell@mitchellopitz.net>
2023-03-31 11:24:03 -0500

    feat - Mushroom art

--- Assets/Scripts/Mushroom.cs
+++ Assets/Scripts/Mushroom.cs
@@ -2,25 +2,35 @@ using UnityEngine;
 
 public class Mushroom : MonoBehaviour
 {
+    public Sprite[] mushroomSprites;
     public int health = 4; // the amount of hits it can take before being destroyed
     public int points = 1;
 
     public bool isPoisoned = false;
 
+    private int currentSpriteIndex = 0;
+    private SpriteRenderer spriteRenderer;
+
     private void Start()
     {
         isPoisoned = false;
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // function to damage the mushroom
     public void TakeDamage()
     {
         health--; // decrease the health by 1
+
         if (health <= 0) // if the health is 0 or less
         {
             FindObjectOfType<ScoreManager>().AddPoints(points);
             FindObjectOfType<MushroomCounter>().Remove();
             Destroy(gameObject); // destroy the current mushroom object
+        } else
+        {
+            currentSpriteIndex++;
+            spriteRenderer.sprite = mushroomSprites[currentSpriteIndex];
         }
     }
 
