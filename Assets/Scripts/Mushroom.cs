1e59fb3c04de5b28d14d9733a9ddb9bb34c4b529
Mitchell Opitz <mitchell@mitchellopitz.net>
2023-03-31 16:12:58 -0500

    style - Poison mushroom art/particles

--- Assets/Scripts/Mushroom.cs
+++ Assets/Scripts/Mushroom.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Mushroom : MonoBehaviour
 {
     public Sprite[] mushroomSprites;
+    public Sprite[] poisonMushroomSprites;
     public int health = 4; // the amount of hits it can take before being destroyed
     public int points = 1;
 
@@ -13,6 +14,7 @@ public class Mushroom : MonoBehaviour
 
     public ParticleSystem redParticles;
     public ParticleSystem greenParticles;
+    public ParticleSystem whiteParticles;
 
     private void Start()
     {
@@ -24,8 +26,15 @@ public class Mushroom : MonoBehaviour
     public void TakeDamage()
     {
         health--; // decrease the health by 1
-        FindObjectOfType<ParticleEffectsManager>().PlayParticleSystem(redParticles, transform.position);
-        FindObjectOfType<ParticleEffectsManager>().PlayParticleSystem(greenParticles, transform.position);
+        if (isPoisoned)
+        {
+            FindObjectOfType<ParticleEffectsManager>().PlayParticleSystem(greenParticles, transform.position);
+            FindObjectOfType<ParticleEffectsManager>().PlayParticleSystem(whiteParticles, transform.position);
+        } else
+        {
+            FindObjectOfType<ParticleEffectsManager>().PlayParticleSystem(redParticles, transform.position);
+            FindObjectOfType<ParticleEffectsManager>().PlayParticleSystem(greenParticles, transform.position);
+        }
 
         if (health <= 0) // if the health is 0 or less
         {
@@ -35,13 +44,21 @@ public class Mushroom : MonoBehaviour
         } else
         {
             currentSpriteIndex++;
-            spriteRenderer.sprite = mushroomSprites[currentSpriteIndex];
+            if (isPoisoned)
+            {
+                spriteRenderer.sprite = poisonMushroomSprites[currentSpriteIndex];
+            } else
+            {
+                spriteRenderer.sprite = mushroomSprites[currentSpriteIndex];
+            }
         }
     }
 
     public void Poison()
     {
         isPoisoned = true;
+
+        spriteRenderer.sprite = poisonMushroomSprites[currentSpriteIndex];
 
         // Temp way to show poison
         GetComponent<SpriteRenderer>().color = new Color(153f / 255f, 1f, 0f, 1f);
