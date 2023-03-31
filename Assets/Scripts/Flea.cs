2a7b438dcc36179ae5459754cfcdb2ced87d1475
Mitchell Opitz <mitchell@mitchellopitz.net>
2023-03-30 22:58:15 -0500

    feat - Flea

---
+++ Assets/Scripts/Flea.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flea : MonoBehaviour
+{
+    public float speed = 2f; // Speed of the flea
+    public float yThreshold = -5f; // Y value below which the flea is destroyed
+    public float mushroomSpawnChance = 0.5f; // Chance of spawning a mushroom at each grid point
+    public float maxMushroomY = 10f;
+    public float minMushroomY = -10f;
+
+    public GameObject mushroomPrefab;
+
+    private Vector2 targetPosition;
+    private Vector2 direction = Vector2.down;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetPosition = GridPosition(transform.position);
+        targetPosition.y += direction.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 currentPosition = transform.position;
+        float step = speed * Time.deltaTime;
+        transform.position = Vector2.MoveTowards(currentPosition, targetPosition, step);
+
+        if (Vector2.Distance(currentPosition, targetPosition) < 0.1f)
+        {
+            // Flea has reached a grid point, check if a mushroom should be spawned
+            if (ShouldSpawnMushroom())
+            {
+                SpawnMushroom(targetPosition);
+            }
+
+            targetPosition = GridPosition(transform.position);
+            targetPosition.y += direction.y;
+
+            // Destroy the flea if it has passed below the y threshold
+            if (transform.position.y < yThreshold)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    private bool ShouldSpawnMushroom()
+    {
+        // Check if a mushroom already exists at the target grid point
+        Collider2D collider = Physics2D.OverlapBox(targetPosition, Vector2.zero, 0f);
+        if (collider != null && collider.GetComponent<Mushroom>() != null)
+        {
+            return false;
+        }
+
+        // Generate a random number between 0 and 1 and compare it to the mushroom spawn chance
+        float random = Random.Range(0f, 1f);
+        return random < mushroomSpawnChance;
+    }
+
+    private void SpawnMushroom(Vector2 position)
+    {
+        if (position.y > minMushroomY && position.y < maxMushroomY)
+        {
+            // Instantiate a new mushroom at the target position
+            Instantiate(mushroomPrefab, position, Quaternion.identity);
+            FindObjectOfType<MushroomCounter>().Add();
+        }
+    }
+
+    private Vector2 GridPosition(Vector2 position)
+    {
+        position.x = Mathf.Round(position.x);
+        position.y = Mathf.Round(position.y);
+        return position;
+    }
+}
---
+++ Assets/Scripts/FleaSpawner.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FleaSpawner : MonoBehaviour
+{
+    public GameObject fleaPrefab;
+    public float spawnDelay = 10f;
+    public int mushroomCountThreshold = 3;
+    public float minX = -6f;
+    public float maxX = 6f;
+    public float spawnY = 11f;
+
+    private MushroomCounter mushroomCounter;
+    private float timer = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mushroomCounter = FindObjectOfType<MushroomCounter>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= spawnDelay && mushroomCounter.mushroomCount < mushroomCountThreshold)
+        {
+            SpawnFlea();
+            timer = 0f;
+        }
+    }
+
+    private void SpawnFlea()
+    {
+        float spawnX = Random.Range(minX, maxX);
+        Vector3 spawnPosition = new Vector3(spawnX, spawnY, 0f);
+        Instantiate(fleaPrefab, spawnPosition, Quaternion.identity);
+    }
+}
