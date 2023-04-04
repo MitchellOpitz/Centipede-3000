6d77e3cb0c1de32c2b940306626b9cfe6b9326f4
Mitchell Opitz <mitchell@mitchellopitz.net>
2023-04-04 12:04:37 -0500

    feat - Leaderboard scene and connection

---
+++ Assets/Scripts/Title/Leaderboard.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Dan.Main;
+
+public class Leaderboard : MonoBehaviour
+{
+    [SerializeField]
+    private List<TextMeshProUGUI> names;
+    [SerializeField]
+    private List<TextMeshProUGUI> scores;
+
+    private string publicLeaderboardKey = "1b1e2bb647b1f858a6ea7822f0f30966ae876ca1c62fffc571144a8122946fbc";
+
+    private void Start()
+    {
+        GetLeaderboard();
+    }
+
+    public void GetLeaderboard()
+    {
+        LeaderboardCreator.GetLeaderboard(publicLeaderboardKey, ((msg) =>
+        {
+            for (int i = 0; i < names.Count; i++)
+            {
+                names[i].text = (i + 1).ToString() + ".) " + msg[i].Username;
+                scores[i].text = msg[i].Score.ToString();
+            }
+        }));
+    }
+
+    public void SetLeaderboardEntry(string username, int score)
+    {
+        LeaderboardCreator.UploadNewEntry(publicLeaderboardKey, username, score, ((msg) =>
+        {
+            GetLeaderboard();
+        }));
+    }
+
+}
