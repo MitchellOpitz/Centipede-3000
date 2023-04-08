e557d411b96ba186fda5ef79a6a937d9c82da41b
Mitchell Opitz <mitchell@mitchellopitz.net>
2023-04-08 08:56:56 -0500

    fix - Default shots / autofire powerup

--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -9,18 +9,49 @@ public class PlayerShoot : MonoBehaviour
 
     private float nextFireTime = 0f;
     private SFXManager sfxManager;
+    private bool autoFireEnabled = false;
+    private float autoFireTime = 5f; // default autofire time is 5 seconds
+    private float autoFireEndTime = 0f;
+
+    // DELETE AFTER POWERUP CREATED
+    private float timeStart;
+    private bool firstTime;
 
     private void Start()
     {
         sfxManager = FindObjectOfType<SFXManager>();
+
+        // DELETE AFTER POWERUP CREATED
+        timeStart = Time.time;
+        firstTime = true;
     }
 
     void Update()
     {
-        if (Input.GetButton("Fire1") && Time.time >= nextFireTime)
+        if (autoFireEnabled && Time.time < autoFireEndTime) // autofire mode
         {
-            Shoot();
-            nextFireTime = Time.time + fireRate;
+            if (Input.GetButton("Fire1") && Time.time >= nextFireTime)
+            {
+                Shoot();
+                nextFireTime = Time.time + fireRate;
+            }
+        }
+        else // normal firing mode
+        {
+            // check if there are any bullets on screen before firing another
+            if (GameObject.FindGameObjectWithTag("Bullet") == null && Input.GetButton("Fire1") && Time.time >= nextFireTime)
+            {
+                Shoot();
+                nextFireTime = Time.time + fireRate;
+            }
+        }
+
+        // DELETE AFTER POWERUP CREATED
+        if (Time.time > timeStart + 10 && firstTime)
+        {
+            EnableAutoFire(5f);
+            Debug.Log("Enabling autofire");
+            firstTime = !firstTime;
         }
     }
 
@@ -30,5 +61,19 @@ public class PlayerShoot : MonoBehaviour
         Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
         rb.velocity = transform.right * bulletSpeed;
         sfxManager.Play("Shoot");
+    }
+
+    public void EnableAutoFire(float time)
+    {
+        autoFireEnabled = true;
+        autoFireTime = time;
+        autoFireEndTime = Time.time + time;
+        nextFireTime = Time.time;
+    }
+
+    // DELETE AFTER POWERUP CREATED (Probably...)
+    public void DisableAutoFire()
+    {
+        autoFireEnabled = false;
     }
 }
